158b8918f4bcb6c5650cd54598e67a9e62069451
Ramnek Chhatwal <ramychha@charter.net>
2021-04-10 11:01:32 -0400

    updating certifications

--- src/components/workandresearch.jsx
+++ src/components/workandresearch.jsx
@@ -22,12 +22,12 @@ export default class Workandresearch extends Component {
                       </div>
                       <div className="workandresearch-label">
                         <h2>Co-op at Itron, Inc. <span>Jan 2019-Aug 2020</span></h2>
-                        <p>I recently completed my 3-semester co-op at Itron working in the Product Management Department but under the role of Networking and Product Management Engineering. Some of my highlights over the 
-                          3 rotations include: 
-                          <li>Developed an integration between Amazon Alexa and Itron meters through a Raspberry Pi using Python and C as the programming languages and Zigbee and Bluetooth as the wireless protocols.</li> 
+                        <p>I recently completed my 3-semester co-op at Itron working in the Product Management Department but under the role of Networking and Product Management Engineering. Some of my highlights over the
+                          3 rotations include:
+                          <li>Developed an integration between Amazon Alexa and Itron meters through a Raspberry Pi using Python and C as the programming languages and Zigbee and Bluetooth as the wireless protocols.</li>
                           <li>Managed all cloud devices and delegated on-site engineers to fix connectivity issues on pilot program to place ~120 Zigbee routers in homes to transmit data to cloud</li>
-                          <li>Put in charge of designing and prototyping hardware to allow ConEd NY to comply with new grid safety and National Electric Code protocols; created a technical document on 
-                            installation, parts, and suppliers</li>  
+                          <li>Put in charge of designing and prototyping hardware to allow ConEd NY to comply with new grid safety and National Electric Code protocols; created a technical document on
+                            installation, parts, and suppliers</li>
                           <li>Travelled to Canada to help analyze and fix manufacturing issues on ~140,000 new meters to be deployed</li>
                         </p>
                       </div>
@@ -41,13 +41,13 @@ export default class Workandresearch extends Component {
                       </div>
                       <div className="workandresearch-label">
                         <h2>Injured Military Veteran Adaptive Sport and Paralympic Soccer Program Development <span>Jan 2018-Present</span></h2>
-                        <p> I have been a part of <a href="http://clemsonparalympicsoccer.org/" target="_blank" rel="noopener noreferrer"> Clemson Paralympic Soccer </a> for most of my 
+                        <p> I have been a part of <a href="http://clemsonparalympicsoccer.org/" target="_blank" rel="noopener noreferrer"> Clemson Paralympic Soccer </a> for most of my
                         time at Clemson, where the focus is on both introducing injured military veterans to adaptive sports as well as working directly with the US Paralympic Soccer Team.<br></br>
-                        With the veterans, we are on our 5th year of a grant that provides us the ability to host 3-4 day camps throughout the year all over the nation that will bring in the veterans together.  
-                        In the camps, we have introduced adaptive sport to the veterans, gave them their grassroots soccer coaching license, and as of this year, mental health first aid certified as well. 
-                        I have participated myself in over 5 camps now helping out with the veterans as well as doing research throughout the camp to collect data surrounding best practices, program evaluation and impact to further improve the program's efforts. I have also now received my Adaptive Recreation and Sports Specialist certification as well as will be receiving my Mental Health First Aid certification later this year.  <br></br>
-                        Another aspect of our program is directly help develop the US Paralympic Soccer Team in preparation for the 2028 Paralympic Games in Los Angeles.  
-                        We are working to start a paralympic soccer team here at Clemson, one of the first teams for a university.  We also work with the head coach of the US team to scout players throughout 
+                        With the veterans, we are on our 5th year of a grant that provides us the ability to host 3-4 day camps throughout the year all over the nation that will bring in the veterans together.
+                        In the camps, we have introduced adaptive sport to the veterans, gave them their grassroots soccer coaching license, and as of this year, mental health first aid certified as well.
+                        I have participated myself in over 5 camps now helping out with the veterans as well as doing research throughout the camp to collect data surrounding best practices, program evaluation and impact to further improve the program's efforts. <br></br>I have also now received a Blaze Sports Adaptive Recreation and Sports Specialist as well as a Mental Health First Aid certification.  <br></br>
+                        Another aspect of our program is directly help develop the US Paralympic Soccer Team in preparation for the 2028 Paralympic Games in Los Angeles.
+                        We are working to start a paralympic soccer team here at Clemson, one of the first teams for a university.  We also work with the head coach of the US team to scout players throughout
                         the country that could be potential candidates for both the Clemson team as well as the US team.<br></br>
                         </p>
                       </div>
@@ -61,9 +61,9 @@ export default class Workandresearch extends Component {
                       </div>
                       <div className="workandresearch-label">
                         <h2>Soccer Referee for US Soccer Federation<span>Aug 2013-Present</span></h2>
-                        <p>I have been a soccer referee since I was 13 years old. Since I played soccer at the time, it made sense to me to get into refereeing as well to be part of a different aspect of the game. 
-                          Throughout my time as a referee, I have been in charge of player safety, enforcing the Laws of the Game, travelling across the country, as well as maintain my physical fitness to keep up with the 
-                          ever increasing speed of play.  I've learned many life skills from being a referee from a young age such as person management, teamwork between my referee crew, as well as confidence from having to 
+                        <p>I have been a soccer referee since I was 13 years old. Since I played soccer at the time, it made sense to me to get into refereeing as well to be part of a different aspect of the game.
+                          Throughout my time as a referee, I have been in charge of player safety, enforcing the Laws of the Game, travelling across the country, as well as maintain my physical fitness to keep up with the
+                          ever increasing speed of play.  I've learned many life skills from being a referee from a young age such as person management, teamwork between my referee crew, as well as confidence from having to
                           stick with my decision with at least the other team and parents disagreeing with me. </p>
                       </div>
                     </div>
@@ -76,7 +76,7 @@ export default class Workandresearch extends Component {
                       </div>
                       <div className="workandresearch-label">
                         <h2>Switched to Computer Science <span>Aug 2019-Present</span></h2>
-                        <p>I eventually made the complete major switch from Computer Engineering to Computer Science with a minor in Mathematical Sciences. I discovered through my classes and some of the projects I worked on at 
+                        <p>I eventually made the complete major switch from Computer Engineering to Computer Science with a minor in Mathematical Sciences. I discovered through my classes and some of the projects I worked on at
                           Itron that I was more interested in working with software development for the future, and changing majors would allow me to do that.
                         </p>
                       </div>
@@ -90,9 +90,9 @@ export default class Workandresearch extends Component {
                       </div>
                       <div className="workandresearch-label">
                         <h2>Robot Control Using MATLAB <span>Aug 2017-Dec 2017</span></h2>
-                        <p>This was my first Creative Inquiry class that I've taken at Clemson and my first time working on a software project with another person. In this class, we used Lego Mindstorms to create 
-                          our own robot and used MATLAB to design a program that will use the Lego Mindstorm sensors to complete and obstacle course. This research project introduced me to engineering and desgin principles 
-                          in a hands-on approach before I took the respective classes later in my curriculum.</p> 
+                        <p>This was my first Creative Inquiry class that I've taken at Clemson and my first time working on a software project with another person. In this class, we used Lego Mindstorms to create
+                          our own robot and used MATLAB to design a program that will use the Lego Mindstorm sensors to complete and obstacle course. This research project introduced me to engineering and desgin principles
+                          in a hands-on approach before I took the respective classes later in my curriculum.</p>
                       </div>
                     </div>
                   </article>
@@ -104,7 +104,7 @@ export default class Workandresearch extends Component {
                       </div>
                       <div className="workandresearch-label">
                         <h2>Started Undergraduate at Clemson University as Computer Engineering<span>Aug 2017-Aug 2019</span></h2>
-                        <p>I started off at Clemson as a Computer Engineering major with a minor is Mathematical Sciences and Computer Science.  This is where I learned mostly about computer hardware as well as low-level 
+                        <p>I started off at Clemson as a Computer Engineering major with a minor is Mathematical Sciences and Computer Science.  This is where I learned mostly about computer hardware as well as low-level
                           programming in C and Assembly. I also completed my first two rotations at Itron as a Computer Engineering Major.
                         </p>
                       </div>
@@ -118,7 +118,7 @@ export default class Workandresearch extends Component {
                       </div>
                       <div className="workandresearch-label">
                         <h2>Completed 45 hours of Dual Enrollment <span>May 2017</span></h2>
-                        <p>I graduated from Brashier Middle College Charter High School while completing 45 college credit hours due to dual enrollment classes.  I played varsity soccer all 4 years I was there and I graduated 
+                        <p>I graduated from Brashier Middle College Charter High School while completing 45 college credit hours due to dual enrollment classes.  I played varsity soccer all 4 years I was there and I graduated
                           Summa Cum Laude.
                         </p>
                       </div>
